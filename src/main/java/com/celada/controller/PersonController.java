94ffbaa17f4a403c85d59b83b3d6aee6f5efecab
Javier Celada <celada.jcm.javier@gmail.com>
2025-03-25 10:17:36 +0100

    Controller and request

---
+++ src/main/java/com/celada/controller/PersonController.java
@@ -0,0 +1,9 @@
+package com.celada.controller;
+
+import com.celada.api.PersonApi;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PersonController implements PersonApi {
+
+}
