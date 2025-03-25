d26247efbec5f0b2bf7a8d37135dacfb831ea795
Javier Celada <celada.jcm.javier@gmail.com>
2025-03-25 12:30:17 +0100

    Attach wife and create children

--- src/main/java/com/celada/db/repository/PersonEntityRepository.java
+++ src/main/java/com/celada/db/repository/PersonEntityRepository.java
@@ -3,5 +3,8 @@ package com.celada.db.repository;
 import com.celada.db.model.PersonEntity;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 
+import java.util.Optional;
+
 public interface PersonEntityRepository extends Neo4jRepository<PersonEntity, Long> {
+    Optional<PersonEntity> findByDni(String dni);
 }
--- src/main/java/com/celada/db/service/PersonNeo4jRepository.java
+++ src/main/java/com/celada/db/service/PersonNeo4jRepository.java
@@ -7,6 +7,10 @@ import com.celada.domain.model.Person;
 import com.celada.domain.repository.PersonRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Repository
 public class PersonNeo4jRepository implements PersonRepository {
 
@@ -27,12 +31,22 @@ public class PersonNeo4jRepository implements PersonRepository {
     @Override
     public Person read(String dni) {
         System.out.println("Read Neo4j");
-        PersonEntity entity = repository.findById(Long.valueOf(dni)).orElseThrow();
+        PersonEntity entity = repository.findByDni(dni).orElseThrow();
         return PersonEntityMapper.INSTANCE.toDomain(entity);
     }
 
     @Override
     public void update(String dni, Person person) {
-
+        System.out.println("Update Neo4j");
+        PersonEntity entity = repository.findByDni(dni).orElseThrow();
+        // Existing wife
+        Optional<PersonEntity> wife = repository.findByDni(person.getWife().getDni());
+        wife.ifPresent(entity::setWife);
+        // Creating children
+        List<PersonEntity> children = person.getChildren().stream()
+                .map(PersonEntityMapper.INSTANCE::toEntity)
+                .collect(Collectors.toList());
+        entity.setChildren(children);
+        repository.save(entity);
     }
 }
