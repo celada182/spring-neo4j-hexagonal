c0a0a6187de78f6d951522be422bbc8f0970c829
Javier Celada <celada.jcm.javier@gmail.com>
2025-04-01 10:39:46 +0200

    First integration tests

--- src/test/java/com/celada/integration/ApplicationTest.java
+++ src/test/java/com/celada/integration/ApplicationTest.java
@@ -1,26 +1,36 @@
 package com.celada.integration;
 
 
+import com.celada.domain.model.Person;
+import org.apache.coyote.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.neo4j.core.Neo4jClient;
-import org.springframework.data.neo4j.core.Neo4jTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 import org.testcontainers.containers.Neo4jContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.net.URI;
+import java.util.Objects;
 
-@SpringBootTest
-@Transactional
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers(disabledWithoutDocker = true)
 public class ApplicationTest {
 
+    @LocalServerPort
+    int port;
+
+    RestTemplate restTemplate = new RestTemplate();
+
     static final Neo4jContainer<?> neo4j = new Neo4jContainer<>("neo4j:4.4")
-            .withPlugins("apoc")
             .withReuse(true);
 
     @DynamicPropertySource
@@ -34,15 +44,26 @@ public class ApplicationTest {
     }
 
     @Test
-    void whatever(@Autowired Neo4jTemplate template, @Autowired Neo4jClient client) {
-        assertThat(template).isNotNull();
-        assertThat(client).isNotNull();
+    void createAndRead() {
+        String dni = "12345678";
+        Person person = Person.builder()
+                .name("Juan")
+                .surname("Perez")
+                .dni(dni)
+                .build();
+        String createUri = String.format("http://localhost:%d/person", port);
+        RequestEntity<Person> createRequest = new RequestEntity<>(person, HttpMethod.POST, URI.create(createUri));
+        ResponseEntity<String> createResponse = restTemplate.exchange(createRequest, String.class);
 
-        String apoc = client
-                .query("RETURN apoc.version() AS output")
-                .fetchAs(String.class)
-                .first()
-                .orElseThrow();
-        assertThat(apoc).startsWith("4.4");
+        assert createResponse.getStatusCode().is2xxSuccessful();
+        assert Objects.equals(createResponse.getBody(), dni);
+
+        String readUri = String.format("http://localhost:%d/person/%s", port, dni);
+        RequestEntity<Void> readRequest = new RequestEntity<>(HttpMethod.GET, URI.create(readUri));
+        ResponseEntity<Person> readResponse = restTemplate.exchange(readRequest, Person.class);
+
+        assert readResponse.getStatusCode().is2xxSuccessful();
+        assert readResponse.getBody() != null;
+        assert Objects.equals(readResponse.getBody().getDni(), person.getDni());
     }
 }
